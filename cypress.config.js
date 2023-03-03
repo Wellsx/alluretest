b0f5e5e98c2a11d49653b851bcd2c04a8567b8e3
Wellsx <stefanveleusic@hotmail.com>
2023-03-03 13:41:47 +0100

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('cypress');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+      // implement node event listeners here
+    },
+    video: false,
+    watchForFileChanges: false,
+    defaultCommandTimeout: 20000,
+    chromewebsecurity: false,
+  },
+});
---
+++ cypress/e2e/pass.cy.js
@@ -0,0 +1,5 @@
+describe('passing test', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io/');
+  });
+});
